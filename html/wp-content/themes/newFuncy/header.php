b6705ab13a2f9ef598fd2239535d73366812baf7
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-11-15 22:14:14 +0100

    Initial commit

---
+++ html/wp-content/themes/newFuncy/header.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1 class='morado'>New Funcy</h1>"
+
+?>
---
+++ html/wp-content/themes/newFuncy/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>New Funcy</title>
+    <link id="hello" type="text/css" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+
+<?php include("header.php"); ?>
+<?php get_header() ?>
+
+<div id="cuerpo"></div>
+<?php
+
+echo "<h1 class='verde'>Hola Funcy</h1>"
+
+?>
+
+<?php get_footer() ?>
+</body>
+</html>
